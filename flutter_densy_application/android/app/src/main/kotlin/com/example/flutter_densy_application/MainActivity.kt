260341c4b139a9ff2aea46c3552e10c4b3133f08
Kitikawin Hompan <65160319@go.buu.ac.th>
2025-02-18 22:21:32 +0700

    <👷build>[flutter-densy-project]:{Recreate project to make it be project for using GetX}

--- flutter_densy_application/android/app/src/main/kotlin/com/example/flutter_densy_application/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.flutter_densy_application
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ flutter_densy_project/android/app/src/main/kotlin/com/densyflutter/flutter_densy_project/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.densyflutter.flutter_densy_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
